e6f75f5a10f867f67c210eb6bf61c0f207bea247
Basar Akkaya <akkayabasar@gmail.com>
2019-02-23 01:44:49 +0300

    changes .info json is updated .sending mail functionality is added .language changing is added

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-//TODO: languageChange event
 //TODO: fix scroll behavior
 //TODO: null check for strings
-//line47
-//line75
-//line161
+//line90 (glyphicon-icon)
+//line193 (add bullet icons)
+//TODO: contact input validation translations
 
 function loadJSON(filePath, success, error) {
-	var xhr = new XMLHttpRequest();
+    var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function()
 	{
-		if (xhr.readyState === XMLHttpRequest.DONE) {
-			if (xhr.status === 200) {
-				if (success)
-					success(JSON.parse(xhr.responseText));
-		} else {
-			if (error)
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                if (success)
+                success(JSON.parse(xhr.responseText));
+            } else {
+                if (error)
 				error(xhr);
 			}
 		}
@@ -42,9 +41,7 @@ function getInfo(forceFetch) {
     });
 }
 
-function fillNavigation(data, contact) {
-    //TODO: Add Navbar Brand Logo and Language Toggle
-
+function fillNavigation(data, contact, languages, currentLanguage) {
     let navigationButtonsHTML = "";
 
     data.forEach(field => {
@@ -63,6 +60,25 @@ function fillNavigation(data, contact) {
         '</li>';
 
         navigationButtonsHTML += contactNavigationButtonHTML;
+    }
+
+    if(languages.length > 1) {
+        let languageTogglers = "";
+
+        languages.forEach(language => {
+            languageTogglers += '<a class="dropdown-item" href="#" onclick="changeLanguage(' + "'" + language + "'" + ')">' + language.toUpperCase() + '</a>'
+        });
+
+        let languageDropdownHTML = 
+        '<li class="nav-item dropdown">' +
+            '<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
+                currentLanguage.toUpperCase() +
+            '</a>' +
+            '<div class="dropdown-menu" aria-labelledby="navbarDropdown">' +
+                languageTogglers +
+            '</div>' +
+        '</li>';
+        navigationButtonsHTML += languageDropdownHTML;
     }
 
     $("#navbar-ul")[0].innerHTML = navigationButtonsHTML;
@@ -84,7 +100,7 @@ function fillJumbotron(data, language) {
             '</div>' +
             '<div class="col-md-8 col-sm-12">' +
                 '<h1 class="display-4" id="full-name">' + data.full_name + '</h1>' +
-                '<p class="lead" id="brief-title">' + data.job_title[language] + '</p>' +
+                '<p class="lead" id="brief-title">' + (data.job_title[language] ? data.job_title[language] : data.job_title["en"]) + '</p>' +
                 '<hr class="my-4">' +
                 '<div id="urls">' +
                     urlsHTML +
@@ -96,7 +112,7 @@ function fillJumbotron(data, language) {
     $("#jumbotron")[0].innerHTML = jumbotronHTML;
 }
 
-function fillContact(data) {
+function fillContact(data, receiverEmail, token) {
     let contactsHTML = 
     '<section id="' + data.field_id + '" class="section">' +
         '<div class="container">' +
@@ -107,14 +123,14 @@ function fillContact(data) {
                     '</div>' +
                     '<div class="row" id="contact-form">' +
                         '<label>' + data.description + '</label>' +
-                        '<form class="col-12">' +
+                        '<form class="col-12" id="contact-form" onsubmit="sendMail(event, ' + "'" + receiverEmail + "'" + ', ' + "'" + token + "'" + ')">' +
                             '<div class="form-group">' +
                                 '<label for="sender-mail-address">' + data.label_sender_mail_address + ' (*)</label>' +
-                                '<input type="email" class="form-control" id="sender-mail-address" placeholder="' + data.placeholder_sender_mail_address + '">' +
+                                '<input type="email" class="form-control" id="sender-mail-address" placeholder="' + data.placeholder_sender_mail_address + '" required>' +
                             '</div>' +
                             '<div class="form-group">' +
                                 '<label for="sender-full-name">' + data.label_sender_fullname + ' (*)</label>' +
-                                '<input type="text" class="form-control" id="sender-full-name" placeholder="' + data.placeholder_sender_fullname + '">' +
+                                '<input type="text" class="form-control" id="sender-full-name" placeholder="' + data.placeholder_sender_fullname + '"required>'  +
                             '</div>' +
                             '<div class="form-group">' +
                                 '<label for="sender-telephone">' + data.label_sender_gsm + '</label>' +
@@ -122,7 +138,7 @@ function fillContact(data) {
                             '</div>' +
                             '<div class="form-group">' +
                                 '<label for="message-body">' + data.label_message_body + ' (*)</label>' +
-                                '<textarea class="form-control" id="message-body" rows="3"></textarea>' +
+                                '<textarea class="form-control" id="message-body" rows="3" required></textarea>' +
                             '</div>' +
                             '<div class="form-group">' +
                                 '<div class="form-check">' +
@@ -130,7 +146,10 @@ function fillContact(data) {
                                     '<label class="form-check-label" for="send-a-copy-to-sender">' + data.label_send_me_a_copy + '</label>' +
                                 '</div>' +
                             '</div>' +
-                            '<button type="submit" class="btn btn-primary" id="button-send-message">' + data.send_button_text + '</button>' +
+                            '<div class="form-message" id="form-success">' + data.form_success_text + '</div>' + 
+                            '<div class="form-message" id="form-fail">' + data.form_fail_text + '</div>' + 
+                            '<button class="btn btn-primary form-button" id="button-send-message" type="submit" >' + data.send_button_text + '</button>' +
+                            '<button class="btn btn-primary form-button" id="button-clear-form" onclick="clearContactForm()">' + data.clear_form_button_text + '</button>' +
                         '</form>' +
                     '</div>' +
                 '</div>' +
@@ -139,6 +158,19 @@ function fillContact(data) {
     '</section>';
 
     $('#sections')[0].innerHTML += contactsHTML;
+/* 
+    $('#sender-mail-address')[0].oninvalid = function() {
+        this.setCustomValidity(data.validity.mail);
+    };
+    $('#sender-full-name')[0].oninvalid = function() {
+        this.setCustomValidity(data.validity.name);
+    };
+    $('#sender-telephone')[0].oninvalid = function() {
+        this.setCustomValidity(data.validity.telephone);
+    };
+    $('#message-body')[0].oninvalid = function() {
+        this.setCustomValidity(data.validity.message);
+    }; */
 }
 
 function fillFieldBulletInfo(bulletInfos){
@@ -168,7 +200,7 @@ function fillFieldBullets(fieldBullets) {
             let bulletHTML = 
             '<li>' + 
                 bullet.field_bullet_title +
-                    (bullet.field_bullet_badge_text && bullet.field_bullet_badge_text !== "" ? (' <span class="badge badge-' + (bullet.field_bullet_badge_is_complete ? "secondary" : "success") + '">' + bullet.field_bullet_badge_text + '</span>') : "") + 
+                    (bullet.field_bullet_badge_text && bullet.field_bullet_badge_text !== "" ? (' <span class="badge badge-' + (bullet.field_bullet_badge_is_complete ? "secondary" : "primary") + '">' + bullet.field_bullet_badge_text + '</span>') : "") + 
                         fillFieldBulletInfo(bullet.field_bullet_infos) + 
             '</li>';
     
@@ -216,15 +248,61 @@ function fillStrings(data, languagePrefix) {
     let language = data.translations[languagePrefix] ? languagePrefix : "en",
     stringsObj = data.translations[language];
 
-    fillNavigation(stringsObj.fields, stringsObj.contact);
+    fillNavigation(stringsObj.fields, stringsObj.contact, Object.keys(data.translations), languagePrefix);
     fillJumbotron(data.personal_info, languagePrefix);
     fillSections(stringsObj.fields);
-    fillContact(stringsObj.contact);
+    fillContact(stringsObj.contact, data.personal_info.receiver_mail_address, data.personal_info.smtpjs_secure_token);
 }
 
 function changeLanguage(languagePrefix) {
     getInfo(false).then((data) => {
         fillStrings(data, languagePrefix);
+    });
+}
+
+function clearContactForm() {
+    $('#sender-mail-address')[0].value = "";
+    $('#sender-full-name')[0].value = "";
+    $('#sender-telephone')[0].value = "";
+    $('#message-body')[0].value = "";
+    $('#send-a-copy-to-sender').prop('checked', false);
+}
+
+function sendMail(e, receiverEmail, token) {
+    e.preventDefault();
+
+    let messageTo = $('#send-a-copy-to-sender').is(':checked') ?  
+        [receiverEmail, $('#sender-mail-address')[0].value] : receiverEmail,
+    messageFrom = receiverEmail,
+    subject = "Contact Mail From: " + $('#sender-mail-address')[0].value,
+    messageBody = 
+    "<p>Contact Mail</p>" + 
+    "<p>Sender Address: " + $('#sender-mail-address')[0].value + ",</p>" + 
+    "<p>Sender Name: " + $('#sender-full-name')[0].value + ",</p>" +
+    "<p>Sender Telephone: " + $('#sender-telephone')[0].value + ",</p>" +
+    "<p>Message: " + $('#message-body')[0].value + "</p><br />" +
+    "<p>Sent at " + new Date() + "</p>";
+
+    Email.send({
+        SecureToken : token, //create from smtpjs
+        To : messageTo,
+        From : messageFrom,
+        Subject : subject,
+        Body : messageBody
+    }).then( function(result) {
+        if(result === "OK"){
+            $('#form-success').addClass('visible');
+            setTimeout(function(){
+                $('#form-success').removeClass('visible');
+                clearContactForm();
+            }, 3000);
+        } else {
+            $('#form-fail').addClass('visible');
+            setTimeout(function(){
+                $('#form-fail').removeClass('visible');
+            }, 3000);
+        }
+        console.log("Email result: " + result);
     });
 }
 
